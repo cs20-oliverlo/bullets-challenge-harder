ba0c16b6b9bf58a785d9cb9fb17f92f6ee8c11d3
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2023-11-29 14:00:10 -0700

    Update main.js

--- main.js
+++ main.js
@@ -98,17 +98,17 @@ function drawCircles(shape, n) {
     if (shape === player) {
         ctx.fillStyle = shape.circleColor;
         ctx.beginPath();
-        ctx.arc(shape.circleX, shape.circleY, shape.circleR, shape.startAngle, shape.endAngle * Math.PI);
+        ctx.arc(shape.xCircle, shape.yCircle, shape.rCircle, shape.startAngle, shape.endAngle * Math.PI);
         ctx.fill();
 
         ctx.strokeStyle = shape.lineColor;
         ctx.lineWidth = shape.lineWidth;
         ctx.beginPath();
         ctx.save();
-        ctx.translate(shape.circleX, shape.circleY);
+        ctx.translate(shape.xCircle, shape.yCircle);
         rotateLine(shape);
         ctx.moveTo(0, 0);
-        ctx.lineTo(0, -shape.circleR);
+        ctx.lineTo(0, -shape.rCircle);
         ctx.stroke();
         ctx.restore();
     }
@@ -128,58 +128,58 @@ function playerControls() {
 
 function playerMovement() {
     if (player.up === true) {
-        player.circleY -= player.yVelocity;
-        player.lineY -= player.yVelocity;
-        player.lineY1 -= player.yVelocity;
+        player.yCircle -= player.yVelocity;
+        player.yLine -= player.yVelocity;
+        player.y1Line -= player.yVelocity;
         player.lastKeyPressed = "w";
     } else if (player.left === true) {
-        player.circleX -= player.xVelocity;
-        player.lineX -= player.xVelocity;
-        player.lineX1 -= player.xVelocity;
+        player.xCircle -= player.xVelocity;
+        player.xLine -= player.xVelocity;
+        player.x1Line -= player.xVelocity;
         player.lastKeyPressed = "a";
     } else if (player.right === true) {
-        player.circleX += player.xVelocity;
-        player.lineX += player.xVelocity;
-        player.lineX1 += player.xVelocity;
+        player.xCircle += player.xVelocity;
+        player.xLine += player.xVelocity;
+        player.x1Line += player.xVelocity;
         player.lastKeyPressed = "d";
     } else if (player.down === true) {
-        player.circleY += player.yVelocity;
-        player.lineY += player.yVelocity;
-        player.lineY1 += player.yVelocity;
+        player.yCircle += player.yVelocity;
+        player.yLine += player.yVelocity;
+        player.y1Line += player.yVelocity;
         player.lastKeyPressed = "s";
     }
 
-    if (player.circleX < 0) {
-        player.circleX = 0;
-        player.lineX = 0;
-        player.lineX1 = 0;
-    } else if (player.circleX > cnv.width) {
-        player.circleX = cnv.width;
-        player.lineX = cnv.width;
-        player.lineX1 = cnv.width;
+    if (player.xCircle < 0) {
+        player.xCircle = 0;
+        player.xLine = 0;
+        player.x1Line = 0;
+    } else if (player.xCircle > cnv.width) {
+        player.xCircle = cnv.width;
+        player.xLine = cnv.width;
+        player.x1Line = cnv.width;
     }
 
-    if (player.circleY < 0) {
-        player.circleY = 0;
-        player.lineY = 0;
-        player.lineY1 = 0;
-    } else if (player.circleY > cnv.height) {
-        player.circleY = cnv.height;
-        player.lineY = cnv.height;
-        player.lineY1 = cnv.height;
+    if (player.yCircle < 0) {
+        player.yCircle = 0;
+        player.yLine = 0;
+        player.y1Line = 0;
+    } else if (player.yCircle > cnv.height) {
+        player.yCircle = cnv.height;
+        player.yLine = cnv.height;
+        player.y1Line = cnv.height;
     }
 }
 
 function playerShoot() {
     if (mouseIsPressed === true && player.reload === 15) {
         if (player.lastKeyPressed === "w") {
-            bullets.push(newBullet(player.circleX, player.circleY, 5, "white", 0, 2, 0, -10));
+            bullets.push(newBullet(player.xCircle, player.yCircle, 5, "white", 0, 2, 0, -10));
         } else if (player.lastKeyPressed === "a") {
-            bullets.push(newBullet(player.circleX, player.circleY, 5, "white", 0, 2, -10, 0));
+            bullets.push(newBullet(player.xCircle, player.yCircle, 5, "white", 0, 2, -10, 0));
         } else if (player.lastKeyPressed === "d") {
-            bullets.push(newBullet(player.circleX, player.circleY, 5, "white", 0, 2, 10, 0));
+            bullets.push(newBullet(player.xCircle, player.yCircle, 5, "white", 0, 2, 10, 0));
         } else if (player.lastKeyPressed === "s") {
-            bullets.push(newBullet(player.circleX, player.circleY, 5, "white", 0, 2, 0, 10));
+            bullets.push(newBullet(player.xCircle, player.yCircle, 5, "white", 0, 2, 0, 10));
         }
 
         player.reload = 0;
@@ -282,16 +282,16 @@ function newBullet(x1, y1, r1, color1, startAngle1, endAngle1, xVelocity1, yVelo
 
 function reset() {
     player = {
-        circleX: canvasMidWidth,
-        circleY: canvasMidHeight,
-        circleR: 25,
+        xCircle: canvasMidWidth,
+        yCircle: canvasMidHeight,
+        rCircle: 25,
         startAngle: 0,
         endAngle: 2,
         circleColor: "white",
-        lineX: canvasMidWidth,
-        lineY: canvasMidHeight,
-        lineX1: canvasMidWidth,
-        lineY1: canvasMidHeight - 25,
+        xLine: canvasMidWidth,
+        yLine: canvasMidHeight,
+        x1Line: canvasMidWidth,
+        y1Line: canvasMidHeight - 25,
         lineWidth: 5,
         lineColor: "red",
         xVelocity: 5,
